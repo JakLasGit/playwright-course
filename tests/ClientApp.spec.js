d13ac228ebf60b76307d525a7d859364a87a59c7
JakLasGit <gitjaklas@gmail.com>
2024-10-08 21:21:14 +0200

    PIERWSZY SAMODZIELNY TEST END2END!!!

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -5,7 +5,6 @@ const lastName = "Peralta"
 const email = "jakeperalta@example.com"
 const password = "Rybaryba123"
 const mobile = "3336669990"
-const creditCardNumber = "1234 4321 1234 4321"
 const cvvCode = "666"
 
 test("Registration Test", async ({ page }) => {
@@ -38,3 +37,36 @@ test("Login Test", async ({ page }) => {
     await expect(firstCard).toHaveText("ZARA COAT 3")
 });
 
+test.only("Adding to card and buying", async ({ page }) => {
+await page.goto("https://rahulshettyacademy.com/client");
+await page.locator("#userEmail").fill(email);
+await page.locator("#userPassword").fill(password);
+await page.locator("[value='Login']").click();
+const addToCartButtons = page.locator("button.w-10");
+await addToCartButtons.first().click();
+await page.locator("[routerlink='/dashboard/cart']").click();
+const itemName = page.locator("div[class='cartSection'] h3")
+await expect(itemName).toHaveText("ZARA COAT 3");
+await page.locator("li[class='totalRow'] button[type='button']").click();
+const shippingEmail = page.locator("[style='color: lightgray; font-weight: 600;']");
+await expect(shippingEmail).toHaveText(email);
+await page.locator("[placeholder='Select Country']").fill("Pola");
+await page.locator("[placeholder='Select Country']").press('n');
+await page.locator("span[class='ng-star-inserted']").click();
+await page.locator("div.field input.txt").nth(1).fill(cvvCode);
+await page.locator("div.field input.txt").nth(2).fill(name);
+await page.locator("div.field input.txt").nth(3).fill("rahulshettyacademy");
+await page.locator("[type='submit']").click();
+const couponText = page.locator("[style='color: green;']");
+await couponText.waitFor();
+await expect(couponText).toHaveText("* Coupon Applied");
+const confirmation = page.locator("h1.hero-primary");
+await page.locator(".action__submit").click();
+await expect(confirmation).toHaveText("Thankyou for the order.")
+let orderID = await page.locator("label.ng-star-inserted").textContent();
+orderID = orderID.replace(/\|/g, '').trim();
+await page.locator("label[routerlink='/dashboard/myorders']").click();
+await page.waitForSelector("tbody tr.ng-star-inserted");
+const allIDs = await page.locator("tbody tr.ng-star-inserted th[scope='row']").allTextContents();
+await expect(allIDs).toContain(orderID);
+});
